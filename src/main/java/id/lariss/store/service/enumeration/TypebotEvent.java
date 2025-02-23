9b7137c7f3b36e58274669701f39f3063a94bc50
doddyph <doddee.ph@gmail.com>
2025-02-23 11:17:20 +0700

    Add search product by cheapest and most expensive intent

--- src/main/java/id/lariss/store/service/enumeration/TypebotEvent.java
+++ src/main/java/id/lariss/store/service/enumeration/TypebotEvent.java
@@ -4,4 +4,7 @@ public enum TypebotEvent {
     SEARCH_PRODUCT,
     VIEW_MY_CART,
     VIEW_MY_ORDER,
+    PRODUCT_DETAILS,
+    ADD_TO_CART,
+    PLACE_ORDER,
 }
--- src/main/java/id/lariss/store/service/impl/v1/TypebotServiceImpl.java
+++ src/main/java/id/lariss/store/service/impl/v1/TypebotServiceImpl.java
@@ -8,8 +8,12 @@ import id.lariss.store.service.v1.AsstCartService;
 import id.lariss.store.service.v1.AsstOrderService;
 import id.lariss.store.service.v1.AsstProductService;
 import id.lariss.store.service.v1.TypebotService;
+import java.util.HashSet;
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,8 @@ import org.springframework.stereotype.Service;
 @Service
 @Log4j2
 public class TypebotServiceImpl implements TypebotService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TypebotServiceImpl.class);
 
     private final OpenAiChatModel chatModel;
     private final AsstProductService productService;
@@ -39,25 +45,7 @@ public class TypebotServiceImpl implements TypebotService {
     public Object handleWebhook(TypebotDTO typebotDTO) {
         switch (typebotDTO.getEvent()) {
             case SEARCH_PRODUCT -> {
-                Prompt prompt = new Prompt(
-                    """
-                        Extract structured data from the following user input.
-                        Return the result in JSON format with the following structure:
-                        {
-                            "productName": string
-                        }
-                        Remove this ```json```
-                        User input: "%s"
-                    """.formatted(typebotDTO.getUserInput())
-                );
-                String content = chatModel.call(prompt).getResult().getOutput().getContent();
-                Map<String, Object> map;
-                try {
-                    map = new ObjectMapper().readValue(content, new TypeReference<>() {});
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-                return productService.searchProduct(String.valueOf(map.get("productName")));
+                return searchProduct(typebotDTO);
             }
             case VIEW_MY_CART -> {
                 return cartService.getCart(typebotDTO.getCustomerId());
@@ -66,8 +54,55 @@ public class TypebotServiceImpl implements TypebotService {
                 return orderService.getOrders(typebotDTO.getCustomerId());
             }
             default -> {
-                return "";
+                return new HashSet<>();
             }
+        }
+    }
+
+    private Object searchProduct(TypebotDTO typebotDTO) {
+        Prompt prompt = new Prompt(promptContent().formatted(typebotDTO.getUserInput()));
+        String content = chatModel.call(prompt).getResult().getOutput().getContent();
+        Map<String, String> contentMap = contentMap(content);
+        LOG.info("--> searchProduct, contentMap: {}", contentMap);
+        String intent = contentMap.getOrDefault("intent", "");
+        String productName = contentMap.getOrDefault("productName", "");
+        if (StringUtils.isNotBlank(intent) && StringUtils.isNotBlank(productName)) {
+            return switch (intent) {
+                case "cheapest" -> productService.findCheapest(productName);
+                case "most expensive" -> productService.findMostExpensive(productName);
+                default -> new HashSet<>();
+            };
+        } else if (StringUtils.isNotBlank(intent)) {
+            return switch (intent) {
+                case "cheapest" -> productService.findCheapest();
+                case "most expensive" -> productService.findMostExpensive();
+                default -> new HashSet<>();
+            };
+        } else {
+            return productService.searchProduct(productName);
+        }
+    }
+
+    private String promptContent() {
+        return """
+            Extract structured data from the following user input.
+            Determine if they are searching for the 'cheapest' or 'most expensive' product.
+            Return only a JSON object. Do not include any markdown formatting, just raw JSON.
+            Ensure the output is structured as follows:
+            {
+                "intent": string with the 'cheapest' or 'most expensive' value,
+                "productName": string
+            }
+            Remove this ```json ```
+            User input: "%s"
+        """;
+    }
+
+    private Map<String, String> contentMap(String content) {
+        try {
+            return new ObjectMapper().readValue(content, new TypeReference<>() {});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
